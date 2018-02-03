de913b2040efcae4637ff7bf7cead4b01e83f8e8
Markus Braunbeck <markus.braunbeck@kaufland.com>
2018-02-03 20:21:01 +0100

    Fix dir name typo

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 
-app.use(express.static(__dirname + '/dest'))
+app.use(express.static(__dirname + '/dist'))
 app.use(bodyParser.urlencoded({ 'extended': 'true' }))
 app.use(bodyParser.json())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 app.use(methodOverride());
 
 app.listen(8080);
-console.log("App listening on port 8080");
+console.log("App listening on port 8080")
