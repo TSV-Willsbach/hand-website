0e0ea164db8d089aa97919ab05a55c027a3eb9dc
Markus Braunbeck <markus.braunbeck@kaufland.com>
2018-02-03 19:59:50 +0100

    Integrate express to serve app files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+
+app.use(express.static(__dirname + '/dest'))
+app.use(bodyParser.urlencoded({ 'extended': 'true' }))
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+app.use(methodOverride());
+
+app.listen(8080);
+console.log("App listening on port 8080");
