7ad5b775b89d2a6a9e4b93c4ceffd605594f6f31
Markus Braunbeck <m.braunbeck@gmx.net>
2018-02-04 13:28:36 +0100

    Refactor footer link with a more dynamic approach
    Footerlinks are now read from an easy to configure json file.
    Link name, url and tool tip can be configured

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { NavigationNode, VersionInfo, NavigationService } from '../navigation/navigation.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  @Input() nodes: NavigationNode[];
+  @Input() versionInfo: VersionInfo;
+  constructor(private navigationService: NavigationService) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.navigationService.versionInfo.subscribe(vi => {
+      this.versionInfo = vi
+    });
+
+    this.navigationService.navigationViews.subscribe(nodes => this.nodes = nodes.Footer);
+
   }
 
 }
