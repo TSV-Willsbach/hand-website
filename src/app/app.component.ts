4a574add515dd3d10fa10f73992ca4c5e602ba83
Markus Braunbeck <m.braunbeck@gmx.net>
2018-02-06 20:07:45 +0100

    Integrate navigation service on global level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { combineLatest } from 'rxjs/observable/combineLatest';
 
-import { NavigationService, NavigationNode, VersionInfo } from './navigation/navigation.service';
+import { NavigationService, NavigationNode, VersionInfo, CurrentNodes } from './navigation/navigation.service';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,23 @@ import { NavigationService, NavigationNode, VersionInfo } from './navigation/nav
 export class AppComponent implements OnInit {
   title = 'app';
   versionInfo: VersionInfo;
+  currentNodes: CurrentNodes;
+  footerNodes: NavigationNode[];
+  topMenuNodes: NavigationNode[];
 
   constructor(
     private navigationService: NavigationService
   ) { }
 
   ngOnInit() {
+    this.navigationService.versionInfo.subscribe(vi => {
+      this.versionInfo = vi
+    });
+
+    this.navigationService.navigationViews.subscribe(views => {
+      this.footerNodes = views['Footer'] || [];
+      this.topMenuNodes = views['TopBar'] || [];
+    });
 
   }
 }
