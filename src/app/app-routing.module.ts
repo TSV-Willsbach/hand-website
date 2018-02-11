41aae54313b094d7df6694e66992321a8a86e596
Markus Braunbeck <m.braunbeck@gmx.net>
2018-02-11 12:28:25 +0100

    Pretty print add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,37 +5,41 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ChronicleComponent } from './chronicle/chronicle.component';
 import { DownloadsComponent } from './downloads/downloads.component';
-import { HomeComponent }  from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { ReportComponent } from './report/report.component';
 import { ResultsComponent } from './results/results.component';
 import { SponsorsComponent } from './sponsors/sponsors.component';
+import { DocViewerComponent } from './doc-viewer/doc-viewer.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },{
+  }, {
     path: 'home',
     component: HomeComponent
-  },{
+  }, {
     path: 'report',
     component: ReportComponent
-  },{
+  }, {
     path: 'about',
     component: AboutComponent
-  },{
+  }, {
     path: 'downloads',
     component: DownloadsComponent
-  },{
+  }, {
     path: 'results',
     component: ResultsComponent
-  },{
+  }, {
     path: 'chronicle',
-    component: ChronicleComponent
-  },{
+    component: DocViewerComponent
+  }, {
     path: 'sponsors',
     component: SponsorsComponent
+  }, {
+    path: 'referee',
+    component: DocViewerComponent
   }
 ];
 
