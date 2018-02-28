8f419f0740af5781a01062dd6d2a32fefd569c86
Markus Braunbeck <m.braunbeck@gmx.net>
2018-02-28 19:21:06 +0100

    Add simple carousel transition

--- src/app/img-carousel/img-carousel.component.ts
+++ src/app/img-carousel/img-carousel.component.ts
@@ -9,11 +9,22 @@ const slidesPath = 'assets/content/slides.json';
 })
 
 export class ImgCarouselComponent implements OnInit {
+  public activeCarousel = 1;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onPrevPress() {
+    this.activeCarousel = this.activeCarousel > 1 ? this.activeCarousel - 1 : 2;
+    console.log(this.activeCarousel)
+  }
+
+  onNextPress() {
+    this.activeCarousel = this.activeCarousel < 2 ? this.activeCarousel + 1 : 1;
+    console.log(this.activeCarousel)
   }
 
 }
