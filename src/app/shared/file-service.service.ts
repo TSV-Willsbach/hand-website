cdcb84b9a263b7f15529057b8bbb4e3dc7fa7ec7
Miggus Studio <support@pontius-pilatus-records.com>
2018-03-20 21:11:59 +0100

    fixed file icons changed dl icon color

--- src/app/shared/file-service.service.ts
+++ src/app/shared/file-service.service.ts
@@ -25,21 +25,38 @@ export class FileServiceService {
     switch (attachment.mime_type) {
       case "application/pdf": {
         attachment.iconColor = "red";
+        attachment.fileType = "PDF";
         icon = "fa-file-pdf-o";
         break;
       }
-      case "application/msword": {
-        attachment.iconColor = "blue";
-        icon = "fa-file-word-o";
-        break;
-      }
-      case "application/msexcel": {
-        attachment.iconColor = "green";
-        icon = "fa-file-excel-o";
-        break;
-      }
+      case "application/msword":
+      case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        {
+          attachment.iconColor = "blue";
+          attachment.fileType = "Word";
+          icon = "fa-file-word-o";
+          break;
+        }
+      case "application/msexcel":
+      case "application/vnd.ms-excel":
+      case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+        {
+          attachment.iconColor = "green";
+          attachment.fileType = "Excel";
+          icon = "fa-file-excel-o";
+          break;
+        }
+      case "application/vnd.ms-powerpoint":
+      case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+        {
+          attachment.iconColor = "orange";
+          attachment.fileType = "Power Point";
+          icon = "fa-file-powerpoint-o";
+          break;
+        }
       default: {
         attachment.iconColor = "black";
+        attachment.fileType = attachment.mime_type.replace("application/", " ");
         icon = "fa-file-o";
       }
     }
