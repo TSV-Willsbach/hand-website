cf514e7a24aa1c49c8f1618a5ba83479aa1f364b
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2018-04-02 02:43:21 +0200

    updated seo meta tags

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { NewsService } from '@wh-share/news.service';
 import { TeamService } from '@wh-share/team.service';
 import { Player } from '@wh-objects/team';
+import { SeoService } from '@wh-share/seo.service';
 
 @Component({
   selector: 'app-player-detail',
@@ -15,14 +16,26 @@ export class PlayerDetailComponent implements OnInit {
   private sub: any;
   player: Player;
 
-  constructor(private route: ActivatedRoute, private httpService: HttpClient, private news: NewsService, teams: TeamService) {
+  constructor(private route: ActivatedRoute, private httpService: HttpClient, private news: NewsService, teams: TeamService, private seo: SeoService) {
     this.player = new Player();
     this.sub = this.route.params.subscribe(params => {
-      teams.getPlayer(params['id'], params['name']).subscribe(player => this.player = player);
-    });
+      teams.getPlayer(params['id'], params['name']).subscribe(
+        player => this.player = player,
+        error => { console.log(error) },
+        () => {
+          this.seo.generateTags({
+            title: this.player.name + ", " + this.player.prename,
+            description: this.player.prename + " " + this.player.name,
+            image: this.player.picture
+          });
+        });
+    }, error => {
+      console.log(error);
+    }, () => { });
   }
 
   ngOnInit() {
+
   }
 
 }
