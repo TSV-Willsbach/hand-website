d4040cbdd31d18db1b04d0ce1035558647575b3c
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2018-04-02 03:49:58 +0200

    changed seo url

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('*', function (req, res) {
     if (isBot) {
         const botUrl = generateUrl(req);
 
-        fetch('${renderUrl}/${botUrl}')
+        fetch(`${renderUrl}/${botUrl}`)
             .then(res => res.text())
             .then(body => {
                 // Cache user agent
