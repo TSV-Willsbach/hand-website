69ea2e83625818740f7fe96ffc6abfd79b017539
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2018-04-16 01:23:59 +0200

    fixed copy error

--- src/app/objects/team-statistics.ts
+++ src/app/objects/team-statistics.ts
@@ -30,7 +30,7 @@ export class teamStatistics {
         statistik.awayWins.procentualWins = statistik.awayWins.wins / statistik.awayWins.gameAmount;
 
         // set value to 0 if undefinded (NaN)
-        statistik.awayGoalsShot.avarageGoals = statistik.awayGoalsShot.avarageGoals || 0;
+        statistik.homeGoalsShot.avarageGoals = statistik.awayGoalsShot.avarageGoals || 0;
         statistik.homeGoalsGot.avarageGoals = statistik.homeGoalsGot.avarageGoals || 0;
         statistik.awayGoalsShot.avarageGoals = statistik.awayGoalsShot.avarageGoals || 0;
         statistik.awayGoalsGot.avarageGoals = statistik.awayGoalsGot.avarageGoals || 0;
