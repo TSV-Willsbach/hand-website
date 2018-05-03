ac9de96b1b0332107635c3c70506d02687453c4f
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2018-05-03 22:50:55 +0000

    environment.ts edited online with Bitbucket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDN0fZC-xUP2VN7TgDTZCfKT6y-bzg0toI",
     authDomain: "web-willsbach-handball.firebaseapp.com",
