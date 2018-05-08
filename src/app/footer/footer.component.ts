a6b56ed39ec2abd408536977a8e7131535107c1d
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2018-05-08 15:25:24 +0200

    changed footer button to fa 5.0 changed icon header and added main club logo to the footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { NavigationNode, VersionInfo } from '../navigation/navigation.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
