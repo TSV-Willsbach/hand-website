bd8d3ab7ab73864d0597508e88f694852c9942ab
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2018-12-27 22:20:00 +0100

    added seo tags for some pages added new icon to seo icons

--- src/app/core/data-protection/data-protection.component.ts
+++ src/app/core/data-protection/data-protection.component.ts
@@ -1,3 +1,4 @@
+import { SeoService } from './../../shared/seo.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataProtectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private seo: SeoService) { }
 
   ngOnInit() {
+    this.seo.generateTags({
+      title: 'Datenschutz',
+      description: 'Datenschutzverordnung'
+    });
   }
 
 }
