d356fcd9a85c35774939aa24d12f8b768c60509c
Miguel Gebhardt <miguel.gebhardt@googlemail.com>
2019-08-08 22:50:09 +0200

    fixed ts lint

--- src/app/shared/file-service.service.ts
+++ src/app/shared/file-service.service.ts
@@ -2,7 +2,6 @@ import { WordpressService } from '@wh-share/wordpress.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Attachment } from '@wh-objects/attachment';
-import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 const apiFiles = 'https://wp.willsbach-handball.de/wp-json/wp/v2/media?_embed&search=downloads';
@@ -10,15 +9,12 @@ const apiFiles = 'https://wp.willsbach-handball.de/wp-json/wp/v2/media?_embed&se
 @Injectable()
 export class FileServiceService {
 
-  constructor(private http: HttpClient, private wp: WordpressService) { }
+  constructor(private wp: WordpressService) { }
 
   fetchFiles(): Observable<Attachment[]> {
-
     return this.wp.getDownloads(false)
       .pipe(
         map(attachments => {
-          console.log(attachments);
-
           return attachments.map(attachment => {
             return this.getIconForType(attachment);
           });
@@ -37,31 +33,28 @@ export class FileServiceService {
         break;
       }
       case 'application/msword':
-      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-        {
-          attachment.iconColor = 'blue';
-          attachment.fileType = 'Word';
-          icon = 'fa-file-word-o';
-          break;
-        }
+      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document': {
+        attachment.iconColor = 'blue';
+        attachment.fileType = 'Word';
+        icon = 'fa-file-word-o';
+        break;
+      }
       case 'application/msexcel':
       case 'application/vnd.ms-excel':
-      case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        {
-          attachment.iconColor = 'green';
-          attachment.fileType = 'Excel';
-          icon = 'fa-file-excel-o';
-          break;
-        }
+      case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': {
+        attachment.iconColor = 'green';
+        attachment.fileType = 'Excel';
+        icon = 'fa-file-excel-o';
+        break;
+      }
       case 'application/vnd.ms-powerpoint':
       case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
-      case 'application/vnd.openxmlformats-officedocument.presentationml.template':
-        {
-          attachment.iconColor = 'orange';
-          attachment.fileType = 'Power Point';
-          icon = 'fa-file-powerpoint-o';
-          break;
-        }
+      case 'application/vnd.openxmlformats-officedocument.presentationml.template': {
+        attachment.iconColor = 'orange';
+        attachment.fileType = 'Power Point';
+        icon = 'fa-file-powerpoint-o';
+        break;
+      }
       default: {
         attachment.iconColor = 'black';
         attachment.fileType = attachment.mime_type.replace('application/', ' ');
