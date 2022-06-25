1e6d40dca73fabdfa0bf6e75b8d27182992711a6
Miguel Gebhardt <miguel@mgm12.de>
2022-06-25 22:11:12 +0200

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,7 +43,8 @@ const routes: Routes = [
   },
   {
     path: "sponsors",
-    component: SponsorsDetailComponent,
+    loadChildren: () =>
+      import("./sponsors/sponsors.module").then((m) => m.SponsorsModule),
   },
   {
     path: "referee",
--- src/app/sponsors/sponsors-routing.module.ts
+++ src/app/sponsors/sponsors-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { SponsorsDetailComponent } from "./sponsors-detail/sponsors-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: SponsorsDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SponsorsRoutingModule { }
+export class SponsorsRoutingModule {}
